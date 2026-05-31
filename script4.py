66c37146b3a90039adf33c10fe165f9765fddd39
Dolik64 <jirka.kosor@gmail.com>
2026-05-31 10:20:57 +0200

    pridavam psia

--- script4.py
+++ script4.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
 
-URL = "https://oiwiki.swpelc.eu/doku.php?id=statnice:bakalar:b0b01lgr"
-OUTPUT_PDF = "oiwiki_b4b36lgr.pdf"
+URL = "https://oiwiki.swpelc.eu/doku.php?id=statnice:bakalar:b4b38psia"
+OUTPUT_PDF = "oiwiki_b4b36psia.pdf"
 
 
 def get_page_metrics(page):
